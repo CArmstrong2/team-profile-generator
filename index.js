8ebdcba90121a2d256d501d4ba43268270ea16ce
Beef Webster <c_goldsmith@yahoo.com>
2022-11-21 17:05:35 -0800

    added inquiries for engineer and intern

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const Intern = require('./Model/Intern')
 const team = []
 
 const start = () =>{
+
     const addManager = () => {
         inquirer
         .prompt([
@@ -41,6 +42,76 @@ const start = () =>{
             userInput.mgrOffice
         )
         team.push(newManager)
+    })
+    }
+
+    const addEngineer = () => {
+        inquirer
+        .prompt([
+            {
+            type:'input',
+            name:'engName',
+            message:`Enter the Engineer's name:`
+        },
+        {
+            type:'input',
+            name:'engID',
+            message:`Enter the Engineer's ID:`
+        },
+        {
+            type:'input',
+            name:'engEmail',
+            message:`Enter the Engineer's email address:`
+        },
+        {
+            type:'input',
+            name:'engOffice',
+            message:`Enter the Engineer's office number:`
+        }
+    ])
+    .then((userInput) => {
+        const newEngineer = new Engineer(
+            userInput.engName,
+            userInput.engID,
+            userInput.engEmail,
+            userInput.engOffice
+        )
+        team.push(newEngineer)
+    })
+    }
+
+    const addIntern = () => {
+        inquirer
+        .prompt([
+            {
+            type:'input',
+            name:'internName',
+            message:`Enter the Intern's name:`
+        },
+        {
+            type:'input',
+            name:'internID',
+            message:`Enter the Intern's ID:`
+        },
+        {
+            type:'input',
+            name:'internEmail',
+            message:`Enter the Intern's email address:`
+        },
+        {
+            type:'input',
+            name:'internOffice',
+            message:`Enter the Intern's office number:`
+        }
+    ])
+    .then((userInput) => {
+        const newIntern = new Intern(
+            userInput.internName,
+            userInput.internID,
+            userInput.internEmail,
+            userInput.internOffice
+        )
+        team.push(newIntern)
     })
     }
 }
